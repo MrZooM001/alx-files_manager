4b1a61b73c98a98ae1e54b0732fbcb35180d4a58
Hazem Magdy <hhazem0010@gmail.com>
2024-06-18 18:38:44 +0300

    files manager - fix returned email after post new user - task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -23,8 +23,8 @@ class UsersController {
     const newUser = { email, password: sha1(password) };
 
     const result = await dbClient.db.collection('users').insertOne(newUser);
-
-    res.status(201).json({ id: result.insertedId.toString(), username: newUser.email });
+    console.log(newUser.email);
+    res.status(201).json({ id: result.insertedId, email: newUser.email });
   }
 }
 
